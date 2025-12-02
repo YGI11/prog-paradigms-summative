864931b5382065332d8949da9699d5d07f69a718
YGI11 <yuvrajgill2004@gmail.com>
2025-12-02 17:16:00 +0000

    passing alignment & allocation/deallocation

--- runme.c
+++ runme.c
@@ -18,20 +18,12 @@ int main(int argc, char *argv[]) {
     //setup
     uint8_t *heap= malloc(heap_size); 
     mm_init(heap, heap_size);
-    if (mm_init(heap, heap_size) != 0) {
-    printf("mm_init failed\n");
-    return 1;
-    }
+    
     
     
 
-    void *ptr1 = mm_malloc(1);
-    void *ptr2 = mm_malloc(41);
-    void *ptr3 = mm_malloc(100);
-    
-    printf("ptr1: %p - mod 40 = %zu\n", ptr1, (size_t)ptr1 % 40);
-    printf("ptr2: %p - mod 40 = %zu\n", ptr2, (size_t)ptr2 % 40);
-    printf("ptr3: %p - mod 40 = %zu\n", ptr3, (size_t)ptr3 % 40);
+    void *ptr1 = mm_malloc(100);
+    printf("ptr1: %p - aligned: %d\n", ptr1, ((size_t)ptr1 % 40) == 0);
 
    
 
